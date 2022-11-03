272b8237adc0ff69b8746766428b82d16c39e9b3
taka <taka@DESKTOP-1FGC2TI.localdomain>
2022-11-03 09:46:55 +0900

    create作成

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -23,7 +23,7 @@ class BookController extends Controller
      */
     public function create()
     {
-        //
+        return view('create');
     }
 
     /**
---
+++ resources/views/create.blade.php
@@ -0,0 +1,69 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="overflow-hidden sm:rounded-lg">
+                <div class="p-6">
+                    <section class="text-gray-600 body-font relative">
+                        <div class="container px-5 pt-4 pb-12 mx-auto">
+                            <div class="flex flex-col text-center w-full mb-12">
+                                <h1 class="sm:text-2xl text-md font-medium">参考書の登録</h1>
+                            </div>
+                            <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                                <div class="flex flex-wrap -m-2">
+                                    <div class="p-2 w-full">
+                                        <div class="relative">
+                                            <label for="title" class="leading-7 text-sm text-gray-600">タイトル</label>
+                                            <input type="text" id="title" name="title" autofocus
+                                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        </div>
+                                    </div>
+                                    <div class="w-full">
+                                        <div class="p-2 w-1/3">
+                                            <div class="relative">
+                                                <label for="due_date" class="leading-7 text-sm text-gray-600">完了期限</label>
+                                                <input type="date" id="due_date" name="due_date"
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="p-2 w-1/3">
+                                        <div class="relative">
+                                            <label for="start_date" class="leading-7 text-sm text-gray-600">開始日</label>
+                                            <input type="date" id="start_date" name="start_date"
+                                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        </div>
+                                    </div>
+                                    <div class="p-2 w-1/3">
+                                        <div class="relative">
+                                            <label for="page" class="leading-7 text-sm text-gray-600">総ページ数</label>
+                                            <input type="number" id="page" name="page"
+                                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        </div>
+                                    </div>
+
+                                    <div class="p-2 w-1/3">
+                                        <div class="relative">
+                                            <label for="page_hour" class="leading-7 text-sm text-gray-600">1時間で読めるページ数</label>
+                                            <input type="number" id="page_hour" name="page_hour"
+                                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        </div>
+                                    </div>
+                                    <div class="p-4 w-full">
+                                        <button
+                                            class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">保存</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
